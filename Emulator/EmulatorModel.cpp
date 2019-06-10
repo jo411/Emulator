d8603f9073cb5a9197fa0a05521a9e0851d4e5cf
Josh Nelson <josh.nelson1@yahoo.com>
2019-06-10 15:25:10 -0600

    Added RAM data structures. ROM loading added.
    The basic skeleton of the RAM is in place. ROMs are loaded into RAM at the correct address.

--- Emulator/EmulatorModel.cpp
+++ Emulator/EmulatorModel.cpp
@@ -1,16 +1,29 @@
 #include "pch.h"
 #include "EmulatorModel.h"
 #include <vector>
+#include <fstream>
+#include <ostream>
+#include <sys/stat.h>
+#include <iostream>
+#include <string>
 
+std::string romName;
+std::string basePath = "../Emulator/Data/";
+
+const int RAM_MAX = 4096;//max ram of 4kb 
+const int dataStartAddr = 0x200;
+unsigned char memory[RAM_MAX];//4kb ram bank
 
 std::vector<int> screen;
 int screenLogicBit = 0; //super temp for basic animation logic
-const char* ROMPath;
 
-EmulatorModel::EmulatorModel(const char* ROMPathIn)
+
+EmulatorModel::EmulatorModel(std::string romNameIn)
 {
-	ROMPath = ROMPathIn;
+	romName = romNameIn;
 	screen.resize(emulatedScreenHeight*emulatedScreenWidth);
+	initialized=loadROMIntoRAM();
+
 }
 
 
@@ -21,6 +34,48 @@ EmulatorModel::~EmulatorModel()
 void EmulatorModel::tick()
 {
 	generateScreenBuffer();
+}
+
+void EmulatorModel::initializeRam()
+{
+	for (int i = 0; i < RAM_MAX; i++)
+	{
+		memory[i] = 0;
+	}
+}
+bool EmulatorModel::loadROMIntoRAM()
+{
+	std::string fullName = basePath + romName;
+
+	struct stat results;
+	if (stat(fullName.c_str(), &results) == 0)
+	{
+		std::cout << "File Opened | Size: " << results.st_size<<"\n";
+	}
+	else
+	{
+		std::cout << "File open error. This file may not exist.\n "<<fullName;
+		return false;//File read error
+	}
+	if (results.st_size > (RAM_MAX - dataStartAddr))
+	{
+		std::cout << "Loaded ROM exceeds memory maximum\n";
+		return false;
+	}
+
+	std::ifstream myFile(fullName,std::ios::in | std::ios::binary);
+	bool status = true;
+	unsigned char* romStartAddr = memory + dataStartAddr;
+
+	myFile.read((char*)romStartAddr,results.st_size);//read all the bytes from ROM to RAM
+	if (!myFile) {
+		std::cout << "File read error. Only read: " << myFile.gcount()<< " bytes.\n";
+		status = false;
+	}
+	myFile.close();
+	
+
+	return status;
 }
 
 void EmulatorModel::generateScreenBuffer() {
@@ -40,4 +95,11 @@ void EmulatorModel::generateScreenBuffer() {
 std::vector<int>  EmulatorModel::getScreenBuffer()
 {
 	return screen;
+}
+
+void EmulatorModel::writeRam()
+{
+	std::ofstream file("RAM.bin", std::ios::out);
+		
+	file.write((char*)memory, RAM_MAX);
 }
--- Emulator/EmulatorModel.h
+++ Emulator/EmulatorModel.h
@@ -5,13 +5,16 @@ class EmulatorModel
 public:
 	int emulatedScreenWidth = 64;
 	int emulatedScreenHeight = 32;
+	bool initialized = false;
 
-	EmulatorModel(const char* ROMPath);
+	EmulatorModel(std::string ROMPath);
 	~EmulatorModel();
 
 	void tick();
 	void generateScreenBuffer();
 	std::vector<int> getScreenBuffer();
-
+	void initializeRam();
+	bool loadROMIntoRAM();
+	void writeRam();
 };
 
